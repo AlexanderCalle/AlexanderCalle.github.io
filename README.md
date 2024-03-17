5397fa2327508a5e1c566ce241336eb44eb54854
AlexanderCalle <callebauta@hotmail.com>
2024-03-17 23:00:13 +0100

    Implementing mockups

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Created Brand
+# Portfolio website
+
+## Info
+
+A site about me as a person, describes who I am, what I can do, my career and projects
---
+++ src/data/skills.ts
@@ -0,0 +1,18 @@
+export default [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Typescript",
+  "React",
+  "Tailwind",
+  "Express",
+  "MySQL",
+  "NoSQL",
+  "Python",
+  "Java",
+  "C#",
+  "Agile development",
+  "OOSD Patterns",
+  "Flutter",
+  "Design"
+]
